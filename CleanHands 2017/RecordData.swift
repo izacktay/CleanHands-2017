67ac85651723dfff1210a54276cb34ffd16f2738
izacktay <15017612@myrp.edu.sg>
2017-05-24 12:57:08 +0800

    set variables for record saving

--- CleanHands 2017/RecordData.swift
+++ CleanHands 2017/RecordData.swift
@@ -12,10 +12,27 @@ class RecordData {
     
     //basically everything in the csv
     // 5 observation point data
+    
+    private var observationPoint : [RecordData] = []
+    
+//    private var date : Date
+    private var location : String = ""
     private var role : String = ""
     private var rank : String = ""
-    private var location : String = ""
+    private var complianceAction : String = ""
     
+    // 5 observation points
+    private var observationMoment : String = ""
+    private var deviceName : String = ""
+    private var note : String = ""
+    private var contact : String = ""
+    private var droplet : String = ""
+    private var airborne : String = ""
+    private var n95Mask : String = ""
+    private var glove : String = ""
+    private var gown : String = ""
+    private var surgicalMask : String = ""
     
+
     
 }
